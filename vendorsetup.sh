805d5cae72141b8c547ed30f9ee5aa11890a44dd
Shirokane Shizuku <nickyundt432@gmail.com>
2023-02-20 15:02:21 +0800

    Re-import vendorsetup.sh from opkona
    Refer to https://github.com/Wishmasterflo/device_oneplus_opkona/blob/android-12.1-oplus/vendorsetup.sh
    Adapt vendorsetup.sh to Android 12.1 to have a better experience on OrangeFox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,61 +1,67 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
-#
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-#
-# 	Please maintain this if you use this script or any part of it
-#
+export TARGET_ARCH="arm64-v8a"
 
-export OF_AB_DEVICE=1
+# Some about us
+#export FOX_VARIANT="A12"
+export FOX_VERSION="R12.1"
+export OF_MAINTAINER=ShionKagurazaka
+
+# Screen
+export OF_SCREEN_H=2520
+# Build environment stuffs
+export FOX_BUILD_DEVICE="pdx203"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export LC_ALL="C"
 export TARGET_DEVICE_ALT="pdx203, XQ-AT52, XQ-AT51, XQ-AT72, XQ-AT42, SO-51A, SOG01, XPERIA 1 II, Xperia 1 II"
 export OF_TARGET_DEVICES="pdx203, XQ-AT52, XQ-AT51, XQ-AT72, XQ-AT42, SO-51A, SOG01, XPERIA 1 II, Xperia 1 II"
-export OF_KEEP_FORCED_ENCRYPTION=1
-export OF_PATCH_AVB20=1
-export OF_SCREEN_H=2520
+
+# Use magisk boot for patching
 export OF_USE_MAGISKBOOT=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
-export OF_NO_MIUI_PATCH_WARNING=1
-export OF_USE_GREEN_LED=0
 
-# use magisk 21.4 for the magisk addon
-#export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+# We have a/b partitions
+export OF_AB_DEVICE="1"
+export OF_VIRTUAL_AB_DEVICE="1"
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION="1"
 
-export FOX_USE_BASH_SHELL=1
-export FOX_ASH_IS_BASH=1
-export FOX_USE_NANO_EDITOR=1
-export FOX_USE_TAR_BINARY=1
-export FOX_USE_ZIP_BINARY=1
-export FOX_USE_SED_BINARY=1
-export FOX_USE_XZ_UTILS=1
-export FOX_REPLACE_BUSYBOX_PS=1
-export OF_USE_NEW_MAGISKBOOT=1
-#export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+# Device stuff
+export OF_KEEP_FORCED_ENCRYPTION="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_FBE_METADATA_MOUNT_IGNORE=1
+export OF_PATCH_AVB20=1
+
+# Run a process after formatting data to work-around MTP issues
+export OF_RUN_POST_FORMAT_PROCESS=1
 
 # OTA for custom ROMs
+export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
 export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
-# let's see what are our build VARs
-if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-fi
+# OTA
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
 
+# For some reason this is dumb and necessary
+export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
+
+# -- add settings for R12.1 --
+export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
+
+# -- add settings for Android 12 --
+# export OF_FIX_DECRYPTION_ON_DATA_MEDIA="1"
+
+# Add some extras
+export FOX_USE_ZIP_BINARY="1"
+export FOX_USE_TAR_BINARY="1"
+export FOX_ASH_IS_BASH="1"
+export FOX_REPLACE_BUSYBOX_PS="1"
+export FOX_USE_BASH_SHELL="1"
+export FOX_USE_LZMA_COMPRESSION="1"
+export FOX_USE_NANO_EDITOR="1"
+export OF_DONT_KEEP_LOG_HISTORY="1"
+export OF_NO_SPLASH_CHANGE="1"
